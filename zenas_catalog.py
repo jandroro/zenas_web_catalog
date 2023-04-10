f25b45002b64e53d74985c1e6bb823e59758eed5
Jano Roberto Camacho Vicente <jandro898.23@gmail.com>
2023-04-09 22:44:02 -0500

    Update zenas_catalog.py

--- zenas_catalog.py
+++ zenas_catalog.py
@@ -27,18 +27,17 @@ option = sl.selectbox('pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comortable, ' + option + ' sweatsuit!'
-sl.write(product_caption)
 
 # Use the option selected to go bacj and get all the info from the database
-#my_cur.execute('SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = ' + option + ';')
-#df2 = my_cur.fetchone()
+my_cur.execute("SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
 
-#sl.image(
-  #df2[0],
-  #width = 400,
-  #caption = product_caption
-#)
+sl.image(
+  df2[0],
+  width = 400,
+  caption = product_caption
+)
 
-#sl.write('Price: ', df2[1])
-#sl.write('Sizes Available: ', df2[2])
-#sl.write(df2[3])
+sl.write('Price: ', df2[1])
+sl.write('Sizes Available: ', df2[2])
+sl.write(df2[3])
