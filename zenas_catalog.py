5c7daee23b808e03cab4262f74b837f042afce2b
Jano Roberto Camacho Vicente <jandro898.23@gmail.com>
2023-04-09 22:27:18 -0500

    Update zenas_catalog.py

--- zenas_catalog.py
+++ zenas_catalog.py
@@ -2,3 +2,14 @@ import streamlit as sl
 import snowflake.connector as sf
 
 sl.title('My web catalog')
+
+# Snowflake
+my_cnx = sf.connect(**sl.secrets['snowflake'])
+
+my_cur = my_cnx.cursor()
+my_cur.execute('SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()')
+
+my_data_row = my_cur.fetchone()
+
+sl.text('Hello from Snowflake:')
+sl.text(my_data_row)
